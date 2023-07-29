ec55489d04d80386fb49595da19d0bf7250fe054
nsopon <sopon101n@gmail.com>
2023-07-29 17:04:15 +0700

    test

--- userApi/Program.cs
+++ userApi/Program.cs
@@ -8,8 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<UserApiDbContext>(option =>
-//option.UseSqlServer(builder.Configuration.GetConnectionString("UserContext")));
-option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+option.UseSqlServer(builder.Configuration.GetConnectionString("UserContext")));
+//option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
